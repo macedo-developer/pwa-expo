84b9ce5c36e720339f6273ece03c3b1bf313fac0
RenataGddoc <renata@gddoc.com.br>
2020-06-30 23:36:36 -0300

    First Commit

---
+++ src/pages/Camera.tsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity, Share, Button } from "react-native";
+import { Camera as ExpoCamera } from "expo-camera";
+import * as Device from "expo-device";
+
+const Camera: React.FC = () => {
+  const [hasPermission, setHasPermission] = useState(false);
+  const [type, setType] = useState(ExpoCamera.Constants.Type.back);
+
+  console.log(Device.brand);
+
+  useEffect(() => {
+    (async () => {
+      if (Device.brand === null) {
+        const retorno = await ExpoCamera.isAvailableAsync();
+        setHasPermission(retorno);
+
+        setType(await ExpoCamera.getAvailableCameraTypesAsync());
+      } else {
+        const { status } = await ExpoCamera.requestPermissionsAsync();
+        setHasPermission(status === "granted");
+      }
+    })();
+  }, []);
+
+  if (hasPermission === false) {
+    return (
+      <View style={{ padding: 24 }}>
+        <Text> Permissão Negada</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <ExpoCamera style={{ flex: 1 }} type={type}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "transparent",
+            flexDirection: "row",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              flex: 0.1,
+              alignSelf: "flex-end",
+              alignItems: "center",
+            }}
+            onPress={() => {
+              setType(
+                type === ExpoCamera.Constants.Type.back
+                  ? ExpoCamera.Constants.Type.front
+                  : ExpoCamera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+              {" "}
+              Flip{" "}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ExpoCamera>
+    </View>
+  );
+};
+
+export default Camera;
---
+++ src/pages/Main.tsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  StyleSheet,
+  Platform,
+} from "react-native";
+
+interface Member {
+  login: string;
+  avatar_url: string;
+}
+
+const Main: React.FC = () => {
+  const [members, setMembers] = useState<Member[]>([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/members").then((response) => {
+      response.json().then((data) => {
+        setMembers(data);
+        console.log(data);
+      });
+    });
+  }, []);
+  console.log(Platform.OS);
+
+  return (
+    <FlatList
+      contentContainerStyle={{ padding: 24 }}
+      style={{ flex: 1 }}
+      data={members}
+      keyExtractor={(member) => member.login}
+      renderItem={({ item: member }) => (
+        <View style={styles.member}>
+          <Image style={styles.image} source={{ uri: member.avatar_url }} />
+          <Text>{member.login}</Text>
+        </View>
+      )}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  member: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  image: {
+    width: 32,
+    height: 32,
+    borderRadius: 42,
+    marginRight: 16,
+  },
+});
+
+export default Main;
